8ef8365ab9101e8a6e1007a4c79265b3163ca77f
Mohammad Farhan R <ramdhanymf@gmail.com>
2022-03-02 10:52:03 +0700

    [feat] check palindrome

--- src/screens/LoginScreen.tsx
+++ src/screens/LoginScreen.tsx
@@ -1,12 +1,55 @@
-import {StyleSheet, Text, View} from 'react-native';
-import React from 'react';
+import React, { useState } from 'react';
+import { StyleSheet, View, Alert } from 'react-native';
+import { RNTestButton, RNTestTextInput } from '../components';
+import { LoginProps } from '../typings/screens';
 
-export default function LoginScreen() {
+export default function LoginScreen({ navigation }: LoginProps) {
+  const [palindrome, setPalindrome] = useState('');
+
+  const checkPalindrome = () => {
+    const re = /[\W_]/g;
+    const lowerRegStr = palindrome.toLowerCase().replace(re, '');
+    const reverseStr = lowerRegStr.split('').reverse().join('');
+
+    return reverseStr === lowerRegStr;
+  };
+
   return (
-    <View>
-      <Text>LoginScreen</Text>
+    <View style={styles.container}>
+      <RNTestTextInput containerStyle={styles.textInput} placeholder="Name" />
+      <RNTestTextInput
+        containerStyle={styles.textInput}
+        placeholder="Palindrome"
+        onChangeText={value => setPalindrome(value)}
+        value={palindrome}
+      />
+
+      <View style={styles.seperator} />
+
+      <RNTestButton buttonStyle={styles.button} label="Check" onPress={checkPalindrome} />
+      <RNTestButton
+        buttonStyle={styles.button}
+        label="Next"
+        onPress={() => navigation.navigate('Home')}
+      />
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingHorizontal: 20,
+  },
+  button: {
+    marginTop: 10,
+  },
+  textInput: {
+    marginBottom: 10,
+  },
+  seperator: {
+    height: 20,
+  },
+});
